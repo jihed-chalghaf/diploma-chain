bbbe765bbedce91e1972915965b29c220b832f8e
JihedCHALGHAF <jihed.chalghaf@insat.u-carthage.tn>
2020-12-21 15:36:01 +0100

    added functionnalities in our main smart contract
    added the functionnalities I thought were necessary for our web app in our Diplomachain smart contract.

--- blockchain/contracts/Diplomachain.sol
+++ blockchain/contracts/Diplomachain.sol
@@ -4,7 +4,7 @@ pragma experimental ABIEncoderV2;
 
 contract Diplomachain {
     struct Diploma {
-        address id;
+        bytes32 id;
         address owner;
         string speciality;
         string honors;
@@ -28,9 +28,12 @@ contract Diplomachain {
     uint256 public studentsCount;
 
     // we can know if a diploma exists or not
-    mapping(address => uint256) public diplomasIndexes;
+    mapping(bytes32 => uint256) public diplomasIndexes;
     Diploma[] public diplomas;
     uint256 public diplomasCount;
+    mapping(bytes32 => uint256) public pendingDiplomasIndexes;
+    Diploma[] public pendingDiplomas;
+    uint256 public pendingDiplomasCount;
 
     // Let's say we have multiple universities, so it's a good practice to define admins addresses for nwo
     mapping(address => uint256) public adminsIndexes;
@@ -38,9 +41,10 @@ contract Diplomachain {
     constructor() {
         studentsCount = 0;
         diplomasCount = 0;
+        pendingDiplomasCount = 0;
     }
 
-    // modifiers declaration
+    // ==========modifiers declaration==========
     modifier onlyAdmin() {
         require(adminsIndexes[msg.sender] != 0, "Admin not found");
         _;
@@ -49,28 +53,94 @@ contract Diplomachain {
         require(studentsIndexes[msg.sender] != 0, "Student not found");
         _;
     }
-    modifier validDiploma() {
-        // converting msg.sender to address type
-        address diploma_adr = msg.sender;
-        require (diplomasIndexes[diploma_adr] != 0, "You provided an invalid diploma identifier");
-        _;
-    }
 
-    // events definition
+    // ==========events definition==========
     event LogRequestDiploma(address studentId);
-    event LogAddDiploma(address diplomaId);
+    event LogAddDiploma(bytes32 diplomaId);
 
-    // Functiosn definition
-    function verifyDiploma(address diplomaId) public view validDiploma returns (Diploma memory){
-        return diplomas[diplomasIndexes[diplomaId]];
+    // ==========Functions definition==========
+    // verifyDiploma()
+    function verifyDiploma(bytes32 diplomaId) public view returns (Diploma memory){
+        require (diplomasIndexes[diplomaId] != 0, "You provided an invalid diploma identifier");
+        return diplomas[diplomasIndexes[diplomaId] - 1];
     }
 
     // addStudent()
+    function addStudent(
+        address _id,
+        string memory _firstName,
+        string memory _lastName,
+        string memory _email,
+        string memory _nationality,
+        string memory _phoneNumber,
+        string memory _gender,
+        Diploma[] memory _diplomas
+    ) public onlyAdmin {
+        require(studentsIndexes[_id] == 0, "Student already exists");
+        studentsCount++;
+        studentsIndexes[_id] = students.length + 1;
+        students.push(
+            Student(_id, _firstName, _lastName, _email, _nationality, _phoneNumber, _gender, _diplomas)
+        );
+    }
     // addDiploma()
+    function addDiploma(bytes32 _id) public onlyAdmin {
+        require(diplomasIndexes[_id] == 0, "Diploma already exists");
+        require(pendingDiplomasIndexes[_id] != 0, "Diploma request is not registered or has been removed");
+        diplomasCount++;
+        diplomasIndexes[_id] = diplomas.length + 1;
+        diplomas.push(
+            pendingDiplomas[pendingDiplomasIndexes[_id] - 1]
+        );
+        emit LogAddDiploma(_id);
+    }
     // isStudent()
+    function isStudent() public view returns (bool) {
+        return studentsIndexes[msg.sender] != 0;
+    }
     // isAdmin()
+    function isAdmin() public view returns (bool) {
+        return adminsIndexes[msg.sender] != 0;
+    }
     // getStudentIndex()
+    function getStudentIndex(address studentId) public view returns (uint256) {
+        require(studentsIndexes[studentId] != 0, "Student does not exist");
+        return studentsIndexes[studentId] - 1;
+    }
     // getDiplomaIndex()
+    function getDiplomaIndex(bytes32 diplomaId) public view returns (uint256) {
+        require(diplomasIndexes[diplomaId] != 0, "Diploma does not exist");
+        return diplomasIndexes[diplomaId] - 1;
+    }
     // getStudents()
+    function getStudents() public onlyAdmin view returns (Student[] memory) {
+        return students;
+    }
     // getDiplomas()
+    function getDiplomas() public onlyAdmin view returns (Diploma[] memory) {
+        return diplomas;
+    }
+    // getStudentDiplomas()
+    function getStudentDiplomas(address studentId) public view returns (Diploma[] memory) {
+        require(studentsIndexes[studentId] != 0, "Student does not exist");
+        require(msg.sender == studentId || adminsIndexes[msg.sender] != 0, "Only owner or admin are allowed");
+        return students[studentsIndexes[studentId] - 1].diplomas;
+    }
+    // requestDiploma()
+    function requestDiploma(
+        string memory _speciality,
+        string memory _honors,
+        string memory _title,
+        uint256 _dateObtained
+    ) public onlyStudent {
+        require(studentsIndexes[msg.sender] != 0, "Student does not exist");
+        // generate a unique hash identifier for each diploma
+        bytes32 id = keccak256(abi.encodePacked(msg.sender, _speciality, _honors, _title, _dateObtained));
+        pendingDiplomasCount++;
+        pendingDiplomasIndexes[id] = pendingDiplomas.length + 1;
+        pendingDiplomas.push(
+            Diploma(id, msg.sender, _speciality, _honors, _title, _dateObtained)
+        );
+        emit LogRequestDiploma(msg.sender);
+    }
 }
